f34f8999b398078628b3d66dcc6a0ecafeb78ff3
v.la <v.la@live.cn>
2018-07-05 18:24:58 +0800

    加入数据生成

---
+++ src/Generator/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using MobilePhoneRegion;
+
+namespace Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filename = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "phones.txt");
+            var dest = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "MobilePhoneRegion.dat");
+
+            using (var fs = File.Create(dest))
+            {
+                MobilePhoneFactory.Generate(MobilePhoneRegion.Version.V1, GetPhoneList(filename).ToArray(), fs);
+            }
+
+            Console.WriteLine("done.");
+        }
+
+        static IList<MobilePhone> GetPhoneList(string filename)
+        {
+            var list = new List<MobilePhone>();
+
+            foreach (var line in File.ReadLines(filename))
+            {
+                var seg = line.Split('|');
+
+                MobilePhone info = new MobilePhone();
+                info.Phone = int.Parse(seg[0]);
+                var province = seg[1];
+                var city = seg[2];
+
+                if (province == city)
+                {
+                    info.Area = city;
+                }
+                else
+                {
+                    info.Area = province + city;
+                }
+
+                info.Isp = seg[3];
+                info.CityCode = seg[4];
+                info.ZipCode = seg[5];
+                info.AdCode = int.Parse(seg[6]);
+
+                list.Add(info);
+            }
+
+            return list;
+        }
+    }
+}
