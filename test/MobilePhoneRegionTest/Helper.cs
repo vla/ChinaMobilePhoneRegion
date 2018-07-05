e1ee72a855572ffe339fc230ea72fc4e30afabc7
v.la <v.la@live.cn>
2018-07-05 18:42:22 +0800

    增加文件有效性测试

---
+++ test/MobilePhoneRegionTest/Helper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using MobilePhoneRegion;
+
+namespace MobilePhoneRegionTest
+{
+    internal class Helper
+    {
+        internal static IList<MobilePhone> GetPhoneList(string filename)
+        {
+            var list = new List<MobilePhone>();
+
+            foreach (var line in File.ReadLines(filename))
+            {
+                var seg = line.Split('|');
+
+                MobilePhone info = new MobilePhone();
+                info.Phone = int.Parse(seg[0]);
+                var province = seg[1];
+                var city = seg[2];
+
+                if (province == city)
+                {
+                    info.Area = city;
+                }
+                else
+                {
+                    info.Area = province + city;
+                }
+
+                info.Isp = seg[3];
+                info.CityCode = seg[4];
+                info.ZipCode = seg[5];
+                info.AdCode = int.Parse(seg[6]);
+
+                list.Add(info);
+            }
+
+            return list;
+        }
+    }
+}
---
+++ test/MobilePhoneRegionTest/MobilePhoneTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using MobilePhoneRegion;
+using Xunit;
+
+namespace MobilePhoneRegionTest
+{
+    public class MobilePhoneTests
+    {
+        [Fact]
+        public void Writer_And_Reader_V1()
+        {
+            var filename = Path.Combine(AppContext.BaseDirectory, "phones.txt");
+            var dest = Path.Combine(AppContext.BaseDirectory, "v1.dat");
+
+            using (var fs = File.Create(dest))
+            {
+                MobilePhoneFactory.Generate(MobilePhoneRegion.Version.V2, Helper.GetPhoneList(filename).ToArray(), fs);
+            }
+
+            var dataSource = new StreamDataSource(dest);
+
+            var searcher = MobilePhoneFactory.GetSearcher(dataSource);
+
+            var result = searcher.Search(1370233);
+
+            Assert.True(result.Success);
+
+            dataSource.Dispose();
+        }
+
+        [Fact]
+        public void Check_ALL_Match_V2()
+        {
+            var filename = Path.Combine(AppContext.BaseDirectory, "phones.txt");
+            var dest = Path.Combine(AppContext.BaseDirectory, "v2.dat");
+            var data = Helper.GetPhoneList(filename).ToArray();
+
+            using (var fs = File.Create(dest))
+            {
+                MobilePhoneFactory.Generate(MobilePhoneRegion.Version.V2, data, fs);
+            }
+
+            var dataSource = new MemoryDataSource(dest);
+
+            var searcher = MobilePhoneFactory.GetSearcher(dataSource);
+            SearchResult result;
+            foreach (var info in data)
+            {
+                result = searcher.Search(info.Phone);
+                Assert.True(result.Success);
+            }
+
+            dataSource.Dispose();
+        }
+    }
+}
