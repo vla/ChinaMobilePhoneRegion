6328afc14255c960c2c118b928ee2c14a202c27f
v.la <v.la@live.cn>
2018-08-15 00:53:11 +0800

    fix: 使用简单正则解决手机号码段验证 #1

--- src/MobilePhoneRegion/MobilePhoneHelper.cs
+++ src/MobilePhoneRegion/MobilePhoneHelper.cs
@@ -8,14 +8,14 @@ namespace MobilePhoneRegion
     public static class MobilePhoneHelper
     {
         //手机号码
-        //移动：134[0-8],135,136,137,138,139,150,151,157,158,159,182,187,188
-        //联通：130,131,132,152,155,156,185,186
-        //电信：133,1349,153,180,189
-        private static readonly Regex MOBILE_Expression = new Regex("^1(3[0-9]|5[0-35-9]|8[025-9])\\d{8}$", RegexOptions.Singleline | RegexOptions.CultureInvariant | RegexOptions.Compiled);
-
-        private static readonly Regex CM_Expression = new Regex("^1(34[0-8]|(3[5-9]|5[017-9]|8[278])\\d)\\d{7}$", RegexOptions.Singleline | RegexOptions.CultureInvariant | RegexOptions.Compiled);
-        private static readonly Regex CU_Expression = new Regex("^1(3[0-2]|5[256]|8[56])\\d{8}$", RegexOptions.Singleline | RegexOptions.CultureInvariant | RegexOptions.Compiled);
-        private static readonly Regex CT_Expression = new Regex("^1((33|53|8[09])[0-9]|349)\\d{7}$", RegexOptions.Singleline | RegexOptions.CultureInvariant | RegexOptions.Compiled);
+        //130 131 132 133 134 135 136 137 138 139 
+        //145 146 147 148 149 
+        //150 151 152 153 155 156 157 158 159 
+        //166 
+        //171 172 173 174 175 176 177 178 
+        //180 181 182 183 184 185 186 187 188 189 
+        //199 198
+        private static readonly Regex MOBILE_Expression = new Regex("^[1][3-9][0-9]{9}$", RegexOptions.Singleline | RegexOptions.CultureInvariant | RegexOptions.Compiled);
 
         /// <summary>
         /// 是否手机号码
@@ -24,10 +24,7 @@ namespace MobilePhoneRegion
         /// <returns></returns>
         public static bool IsMobilePhone(string input)
         {
-            return MOBILE_Expression.IsMatch(input)
-                 || CM_Expression.IsMatch(input)
-                 || CU_Expression.IsMatch(input)
-                 || CT_Expression.IsMatch(input);
+            return MOBILE_Expression.IsMatch(input);
         }
 
         /// <summary>
---
+++ test/MobilePhoneRegionTest/Issues1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using MobilePhoneRegion;
+using Xunit;
+
+namespace MobilePhoneRegionTest
+{
+    public class Issues1
+    {
+        [Fact]
+        public void Check_Mobile17X()
+        {
+            var searcher = MobilePhoneFactory.GetSearcher();
+
+            var result = searcher.TryGet("17093318888", out MobilePhone info);
+            Assert.True(result);
+        }
+    }
+}
