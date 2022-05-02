aa9b0a8b6d30da1a91d3f40e871a866dc2276d53
v.la <v.la@live.cn>
2022-05-02 22:12:41 +0800

    chore: bump to 1.0.8

--- build.cmd
+++ build.cmd
@@ -4,7 +4,7 @@ if exist %artifacts%  rd /q /s %artifacts%
 
 call dotnet restore src/MobilePhoneRegion
 
-call dotnet build src/MobilePhoneRegion -f net40 -c Release -o %artifacts%\net40
+call dotnet build src/MobilePhoneRegion -f net462 -c Release -o %artifacts%\net462
 call dotnet build src/MobilePhoneRegion -f netstandard2.0 -c Release -o %artifacts%\netstandard2.0
 
 call dotnet pack src/MobilePhoneRegion -c release -o %artifacts%
