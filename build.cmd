0766e0d7c097542bd05d6d07f5cdbc496019e869
v.la <v.la@live.cn>
2025-09-10 12:16:07 +0800

    build: 支持版本构建

--- build.cmd
+++ build.cmd
@@ -2,9 +2,10 @@ set artifacts=%~dp0artifacts
 
 if exist %artifacts%  rd /q /s %artifacts%
 
+set /p ver=<VERSION
 call dotnet restore src/MobilePhoneRegion
 
 call dotnet build src/MobilePhoneRegion -f net462 -c Release -o %artifacts%\net462
 call dotnet build src/MobilePhoneRegion -f netstandard2.0 -c Release -o %artifacts%\netstandard2.0
 
-call dotnet pack src/MobilePhoneRegion -c release -o %artifacts%
+call dotnet pack src/MobilePhoneRegion -c release -p:Ver=%ver% -o %artifacts%
